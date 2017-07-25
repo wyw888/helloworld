1eee07b63b52561dd6b8badc81c3f8f0af575da8
wyw888 <wyw888@126.com>
2017-07-25 17:02:02 +0800

    第二次

--- src/wywtest/test.java
+++ src/wywtest/test.java
@@ -3,7 +3,11 @@ package wywtest;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 		System.out.println("hello world");
 
 	}
